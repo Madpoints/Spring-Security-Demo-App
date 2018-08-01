d68b3e2c4556eb326be680666b7b4a4c76e7b1d9
John Furtado <jf8846@gmail.com>
2018-07-31 20:02:23 -0400

    updated spring security config to restrict access based on user roles

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -31,15 +31,17 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.anyRequest().authenticated()
+				.antMatchers("/").hasRole("EMPLOYEE")
+				.antMatchers("/leaders/**").hasRole("MANAGER")
+				.antMatchers("/systems/**").hasRole("ADMIN")
 			.and()
-			.formLogin()
-			.loginPage("/loginPage")
-			.loginProcessingUrl("/authenticateUser")
-			.permitAll()
+				.formLogin()
+				.loginPage("/loginPage")
+				.loginProcessingUrl("/authenticateUser")
+				.permitAll()
 			.and()
-			.logout()
-			.permitAll();
+				.logout()
+				.permitAll();
 		
 	}
 	

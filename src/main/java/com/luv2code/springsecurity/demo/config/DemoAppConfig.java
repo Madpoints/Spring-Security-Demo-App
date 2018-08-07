fde283dd9396c0d7d30b70363e0c1bf5f59b7abc
John Furtado <jf8846@gmail.com>
2018-08-07 16:43:29 -0400

    configured datasource

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,19 +1,34 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo.")
 @PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
-
+	
+	// holds data read from properties file
+	@Autowired
+	private Environment env;
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 	// bean for ViewResolver
 	@Bean
 	public ViewResolver viewResolver() {
@@ -24,5 +39,51 @@ public class DemoAppConfig {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+	
+	// bean for security datasource
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// connection pool
+		ComboPooledDataSource securityDataSource
+			= new ComboPooledDataSource();
+		
+		// jdbc driver class
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		logger.info("*** jdbc.url= " + env.getProperty("jdbc.url"));
+		logger.info("*** jdbc.user= " + env.getProperty("jdbc.user"));
+		
+		// database connection properties
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.user"));
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.password"));
+
+		// connection pool properties
+		securityDataSource.setInitialPoolSize(
+				getIntProperty("connection.pool.initialPoolSize"));
+		
+		securityDataSource.setMinPoolSize(
+				getIntProperty("connection.pool.minPoolSize"));
+
+		securityDataSource.setMaxPoolSize(
+				getIntProperty("connection.pool.maxPoolSize"));
+		
+		securityDataSource.setMaxIdleTime(
+				getIntProperty("connection.pool.maxIdleTime"));
+		
+		
+		return securityDataSource;
+	}
+	
+	// converts env property to int
+	private int getIntProperty(String propName) {
+		
+		return Integer.parseInt(env.getProperty(propName));
 	}
 }

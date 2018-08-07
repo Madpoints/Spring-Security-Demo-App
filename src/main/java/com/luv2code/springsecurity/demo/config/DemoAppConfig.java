e6eae71d0f1ae1713b458255ecf311588028726f
John Furtado <jf8846@gmail.com>
2018-08-07 15:23:15 -0400

    property source notation to read jdbc properties file

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -3,6 +3,7 @@ package com.luv2code.springsecurity.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo.")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 
 	// bean for ViewResolver

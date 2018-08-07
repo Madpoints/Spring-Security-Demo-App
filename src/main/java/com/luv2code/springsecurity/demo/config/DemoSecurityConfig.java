ef6fb60131cd492a1397f36f3cce00a26066f149
John Furtado <jf8846@gmail.com>
2018-08-07 16:49:51 -0400

    removed unused imports

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +15,12 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private DataSource securityDataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
